4c97cd80ab129f4f9aa210192a73717f446712a0
GuinhoFSilva <guilherme.silva@sptech.school>
2025-02-24 19:01:47 -0300

    Iniciando a verificação de credenciais de login

--- public/js/scriptLogin.js
+++ public/js/scriptLogin.js
@@ -1,3 +1,38 @@
+function validarLogin(){
+    var email = document.getElementById('iptemail').value;
+    var mensagemErro = "";
+    var tamanhoValido = false;
+    
+    if(!email){
+        mensagemErro = ` `
+    }
+    if(!email.includes('@')){
+        mensagemErro += `O email deve incluir '@'<br>`
+    }
+    if(!email.includes('.')){
+        mensagemErro += `O email deve incluir ponto '.'<br>`
+    }
+    if(email.includes(' ')){
+        mensagemErro += `O email não pode conter espaços<br>`
+    }
+    if (email.length < 5){
+        mensagemErro += `O email deve ter no mínimo 5 caracteres<br>`
+    }else if(email.length > 100){
+        mensagemErro += `O email deve ter no máximo 100 caracteres<br>`
+    }else{
+        tamanhoValido = true;
+    }
+
+    divmsg.innerHTML = mensagemErro
+
+    if(email.includes('@') && email.includes('.') && !email.includes(' ') && tamanhoValido){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
 function logar(){
     var codigo = document.getElementById('iptcodigo').value;
     var email = document.getElementById('iptemail').value;
@@ -7,12 +42,13 @@ function logar(){
     if(!email || !senha || !codigo){
         mensagemErro = "Informe todos os campos!"
     }
-
-    fetch("/usuarios/autenticar", {
-        method: "POST",
+    
+    if(validarLogin(email)){
+        fetch("/usuarios/autenticar", {
+            method: "POST",
         headers: {
-                "Content-Type": "application/json"
-            },
+            "Content-Type": "application/json"
+        },
             body: JSON.stringify({
                 emailServer: email,
                 senhaServer: senha,
@@ -20,7 +56,7 @@ function logar(){
             }),
         }).then(function (resposta) {
             console.log("ESTOU NO THEN DO entrar()!", resposta)
-
+            
             if (resposta.ok) {
                 resposta.json().then(json => {
                     console.log(json);
@@ -39,6 +75,5 @@ function logar(){
         })
 
         return false;
-
-
+    }
 }
