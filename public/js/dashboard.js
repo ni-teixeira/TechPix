f46037b7c64bc33ff535115acdb6a144b00e6480
ni-teixeira <nicoly.teixeira@sptech.school>
2025-03-13 00:12:54 -0300

    graficos dentro do modal

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -26,3 +26,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function carregarGraficos() {
+    const graficos = [
+        { id: 'donut1', data: [80], label: 'Porcentagem da CPU' },
+        { id: 'donut2', data: [60], label: 'Interrupções do sistema' },
+        { id: 'donut3', data: [40], label: 'Interrupções de softwares' },
+        { id: 'donut4', data: [20], label: 'Frequência da CPU' }
+    ];
+
+    graficos.forEach(grafico => {
+        const ctx = document.getElementById(grafico.id);
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    label: grafico.label,
+                    data: grafico.data,
+                    backgroundColor: ['#4868A5', '#899EC9'],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        labels: {
+                            color: '#ffffff',
+                            font: {
+                                size: 18
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    });
+}
